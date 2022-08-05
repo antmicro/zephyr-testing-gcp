05385990840e9036e87feabeb75706cb6fecf937
Krzysztof Sychla <ksychla@antmicro.com>
2022-08-05 11:36:55 +0200

    Add zephyr preparation

---
+++ scripts/prepare_zephyr.sh
@@ -0,0 +1,13 @@
+pip uninstall -y devicetree
+west init zephyrproject 1>artifacts/build.log 2>&1
+cd zephyrproject/zephyr
+git checkout ${ZEPHYR_VERSION}
+git apply ../../patches/zephyr/*.patch
+pip3 install -r scripts/requirements.txt 1>>../../artifacts/build.log 2>&1
+cd scripts/dts/python-devicetree
+python3 setup.py install
+cd ../../../../
+for i in $(seq 1 5); do west update 1>>../artifacts/build.log 2>&1 && break || sleep 5; done
+west espressif install 1>>../artifacts/build.log 2>&1
+cd ..
+

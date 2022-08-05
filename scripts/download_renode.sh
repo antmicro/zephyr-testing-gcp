2487fb4a7adea9b5f12ff0cefe32f804be7e5a42
Krzysztof Sychla <ksychla@antmicro.com>
2022-08-05 10:15:58 +0200

    Add renode download

---
+++ scripts/download_renode.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p renode_portable && cd renode_portable
+curl -kL https://dl.antmicro.com/projects/renode/builds/renode-${RENODE_VERSION}.linux-portable.tar.gz | tar xz --strip 1
+ln -s ../artifacts artifacts
+export PATH=`pwd`:$PATH
+cd -
+
--- scripts/test.py
+++ scripts/test.py
@@ -5,3 +5,4 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         exit(1)
     print(sys.argv[1])
+

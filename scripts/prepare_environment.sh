f240ef04820effe58eb7e61094a023d855ef6602
Krzysztof Sychla <ksychla@antmicro.com>
2022-08-05 11:02:32 +0200

    Update prepare_environment.sh

--- scripts/prepare_environment.sh
+++ scripts/prepare_environment.sh
@@ -2,9 +2,9 @@
 
 mkdir -p artifacts
 sudo apt -qqy update > /dev/null 2> /dev/null
-echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+sudo echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 sudo apt -qqy install curl git cmake ninja-build gperf ccache dfu-util device-tree-compiler wget python3-dev python3-pip python3-setuptools python3-tk python3-wheel xz-utils file make gcc gcc-multilib g++-multilib libsdl2-dev 1>/dev/null 2>/dev/null
 pip3 install -qr requirements.txt
 export PATH=$HOME/.local/bin:"$PATH"
-update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
 

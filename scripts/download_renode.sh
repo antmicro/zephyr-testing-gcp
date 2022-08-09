aa7bdb84344148a20d18280f0cc29246f0c72645
Krzysztof Sychla <ksychla@antmicro.com>
2022-08-09 09:06:33 +0200

    Add debug ls and pwd

--- scripts/download_renode.sh
+++ scripts/download_renode.sh
@@ -3,6 +3,6 @@
 mkdir -p renode_portable && cd renode_portable
 curl -kL https://dl.antmicro.com/projects/renode/builds/renode-${RENODE_VERSION}.linux-portable.tar.gz | tar xz --strip 1
 ln -s ../artifacts artifacts
-export PATH=`pwd`:$PATH
+#export PATH=`pwd`:$PATH
 cd -
-
+ls -la ronode_portable/

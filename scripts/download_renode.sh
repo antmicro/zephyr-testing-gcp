30004d60558ff0f31cd246fc8ccb87dea8da8d9e
Krzysztof Sychla <ksychla@antmicro.com>
2022-08-09 09:28:31 +0200

    Fix adding renode to path

--- scripts/download_renode.sh
+++ scripts/download_renode.sh
@@ -3,6 +3,5 @@
 mkdir -p renode_portable && cd renode_portable
 curl -kL https://dl.antmicro.com/projects/renode/builds/renode-${RENODE_VERSION}.linux-portable.tar.gz | tar xz --strip 1
 ln -s ../artifacts artifacts
-#export PATH=`pwd`:$PATH
+echo `pwd` >> $GITHUB_PATH
 cd -
-ls -la renode_portable/

e929fba34c25ba1441fb24cec7cf27a1db2b0f75
Krzysztof Sychla <ksychla@antmicro.com>
2022-08-16 07:51:03 +0200

    Change renode requirements

--- scripts/download_renode.sh
+++ scripts/download_renode.sh
@@ -2,6 +2,7 @@
 
 mkdir -p renode_portable && cd renode_portable
 curl -kL https://dl.antmicro.com/projects/renode/builds/renode-${RENODE_VERSION}.linux-portable.tar.gz | tar xz --strip 1
+pip3 install -r tests/requirements.txt
 ln -s ../artifacts artifacts
 echo `pwd` >> $GITHUB_PATH
 cd -

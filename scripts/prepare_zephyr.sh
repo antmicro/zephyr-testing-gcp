190265405d41de6e7d8f422e3523b0f71abc1c3f
Krzysztof Sychla <ksychla@antmicro.com>
2022-08-16 11:23:55 +0200

    Code review changes v2

--- scripts/prepare_zephyr.sh
+++ scripts/prepare_zephyr.sh
@@ -14,7 +14,7 @@ cd zephyrproject/zephyr
 git checkout ${ZEPHYR_COMMIT}
 git apply ../../patches/zephyr/*.patch
 pip3 install -r scripts/requirements.txt 1>>../../artifacts/build.log 2>&1
-cd ../../../../
+cd ../
 for i in $(seq 1 5); do west update 1>>../artifacts/build.log 2>&1 && break || sleep 5; done
 west espressif install 1>>../artifacts/build.log 2>&1
 cd ..

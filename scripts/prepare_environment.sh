d66be52e1ab1d36f3528aeb4918ec1f69e5cf30c
Krzysztof Sychla <ksychla5@gmail.com>
2022-08-23 13:54:17 +0200

    Add setuptools

--- scripts/prepare_environment.sh
+++ scripts/prepare_environment.sh
@@ -7,3 +7,4 @@ mkdir -p artifacts/results
 apt -qqy update
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 apt -qqy install curl git cmake ninja-build gperf ccache dfu-util device-tree-compiler wget python3-dev python3-pip python3-setuptools python3-tk python3-wheel xz-utils file make gcc gcc-multilib g++-multilib libsdl2-dev
+pip3 install setuptools

43f2964d2696f39bd93a0b97f399ed76d181a65e
Krzysztof Sychla <ksychla5@gmail.com>
2022-08-23 13:35:51 +0200

    Change pip to pip3

--- scripts/prepare_zephyr.sh
+++ scripts/prepare_zephyr.sh
@@ -4,12 +4,12 @@ set -u
 set -x
 
 tar xf zephyr-artifact/zephyr.tar
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 cd zephyr-sdk
 ./setup.sh -t all -h -c
 cd -
 cd zephyrproject/zephyr
-pip install -r scripts/requirements.txt 1>>../../artifacts/build.log 2>&1
+pip3 install -r scripts/requirements.txt 1>>../../artifacts/build.log 2>&1
 cd -
 rm -rf zephyr-artifact
 

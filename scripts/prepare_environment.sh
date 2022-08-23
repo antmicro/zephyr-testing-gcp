f1a397b9b1fbc8aa6ef75b88f079df0cadfdb9dd
Krzysztof Sychla <ksychla5@gmail.com>
2022-08-23 12:57:54 +0200

    Remove update-alternatives

--- scripts/prepare_environment.sh
+++ scripts/prepare_environment.sh
@@ -7,5 +7,5 @@ mkdir -p artifacts/results
 apt -qqy update
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 apt -qqy install curl git cmake ninja-build gperf ccache dfu-util device-tree-compiler wget python3-dev python3-pip python3-setuptools python3-tk python3-wheel xz-utils file make gcc gcc-multilib g++-multilib libsdl2-dev
-update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
+#update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
 

98c9179f30753a72b3f072b28de8eec13718d283
Krzysztof Sychla <ksychla5@gmail.com>
2022-08-24 09:42:37 +0200

    Update ubuntu version v2

--- scripts/prepare_environment.sh
+++ scripts/prepare_environment.sh
@@ -5,7 +5,7 @@ set -x
 
 mkdir -p artifacts/results
 apt -qqy update
-apt install software-properties-common
+apt -qqy install software-properties-common
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 apt -qqy install curl git cmake ninja-build gperf ccache dfu-util device-tree-compiler wget python3-dev python3-pip python3-setuptools python3-tk python3-wheel xz-utils file make gcc gcc-multilib g++-multilib libsdl2-dev
 pip3 install --upgrade pip

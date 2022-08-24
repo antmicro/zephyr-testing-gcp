b8973bbd56440ecc999a3c5e6f800e327487acf0
Krzysztof Sychla <ksychla5@gmail.com>
2022-08-24 09:17:38 +0200

    Remove python3.6

--- scripts/prepare_environment.sh
+++ scripts/prepare_environment.sh
@@ -6,6 +6,7 @@ set -x
 mkdir -p artifacts/results
 apt -qqy update
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+apt -qqy remove python3.6
 apt -qqy install curl git cmake python3.10 ninja-build gperf ccache dfu-util device-tree-compiler wget python3-dev python3-pip python3-setuptools python3-tk python3-wheel xz-utils file make gcc gcc-multilib g++-multilib libsdl2-dev
 pip3 install --upgrade pip
 ls -la /usr/bin/pyth*

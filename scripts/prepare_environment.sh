67fea808506c636d8edf86d33081f6df695bd8e0
Krzysztof Sychla <ksychla5@gmail.com>
2022-08-24 09:11:00 +0200

    Add python version debug

--- scripts/prepare_environment.sh
+++ scripts/prepare_environment.sh
@@ -8,4 +8,5 @@ apt -qqy update
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 apt -qqy install curl git cmake python3.10 ninja-build gperf ccache dfu-util device-tree-compiler wget python3-dev python3-pip python3-setuptools python3-tk python3-wheel xz-utils file make gcc gcc-multilib g++-multilib libsdl2-dev
 pip3 install --upgrade pip
+ls -la /usr/bin/pyth*
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1

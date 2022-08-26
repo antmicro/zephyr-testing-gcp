e817ad4498ab75a380e525fa3ea31dae5fadf82a
Krzysztof Sychla <ksychla5@gmail.com>
2022-08-26 12:35:13 +0200

    Add GITHUB_PATH again

--- scripts/download_renode.sh
+++ scripts/download_renode.sh
@@ -6,6 +6,7 @@ set -x
 mkdir -p renode_portable && cd renode_portable
 curl -kL https://dl.antmicro.com/projects/renode/builds/renode-${RENODE_VERSION}.linux-portable.tar.gz | tar xz --strip 1
 ln -s ../artifacts artifacts
+echo `pwd` >> $GITHUB_PATH
 pip3 install -r tests/requirements.txt
 cd -
 pip3 install -r requirements_simulate.txt

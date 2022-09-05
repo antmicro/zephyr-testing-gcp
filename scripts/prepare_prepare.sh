95dee6ae05bc958a40fc2cd1a0449c9a02185f02
Krzysztof Sychla <ksychla5@gmail.com>
2022-09-05 10:50:35 +0200

    Add wget to prepare script

--- scripts/prepare_prepare.sh
+++ scripts/prepare_prepare.sh
@@ -5,5 +5,5 @@ set -x
 
 apt -qqy update
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
-apt -qqy install curl git cmake python3-dev python3-pip python3-setuptools python3-tk python3-wheel
+apt -qqy install curl git cmake wget python3-dev python3-pip python3-setuptools python3-tk python3-wheel
 pip3 install --upgrade pip

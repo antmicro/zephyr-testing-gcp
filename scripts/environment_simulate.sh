fd836f1436cede10858ba454d03121cadc5d4c2c
Krzysztof Sychla <ksychla5@gmail.com>
2022-09-05 13:58:47 +0200

    Add git installation to environment preparation

--- scripts/environment_simulate.sh
+++ scripts/environment_simulate.sh
@@ -10,6 +10,6 @@ echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 apt -qqy update
-apt -qqy install python3-dev python3-pip python3-setuptools python3-tk python3-wheel google-cloud-cli
+apt -qqy install git python3-dev python3-pip python3-setuptools python3-tk python3-wheel google-cloud-cli
 pip3 install --upgrade pip
 pip3 install -r requirements_simulate.txt

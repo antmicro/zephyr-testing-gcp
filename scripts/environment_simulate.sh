c9291ab6ff9daf45d744eb91c84e128ff3d6d012
Krzysztof Sychla <ksychla5@gmail.com>
2022-09-05 13:26:02 +0200

    Move gnupg installation

--- scripts/environment_simulate.sh
+++ scripts/environment_simulate.sh
@@ -5,11 +5,11 @@ set -x
 
 mkdir -p artifacts/results
 apt -qqy update
-apt -qqy install curl
+apt -qqy install curl gnupg
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 apt -qqy update
-apt -qqy install python3-dev python3-pip python3-setuptools python3-tk python3-wheel gnupg google-cloud-cli
+apt -qqy install python3-dev python3-pip python3-setuptools python3-tk python3-wheel google-cloud-cli
 pip3 install --upgrade pip
 pip3 install -r requirements_simulate.txt

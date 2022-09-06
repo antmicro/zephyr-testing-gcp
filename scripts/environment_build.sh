4cf53c2d2dd86ed40f26b9ace2295d2087b16d70
Krzysztof Sychla <ksychla5@gmail.com>
2022-09-06 09:16:29 +0200

    Add ninja to environment build

--- scripts/environment_build.sh
+++ scripts/environment_build.sh
@@ -5,6 +5,6 @@ set -x
 
 apt -qqy update
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
-apt -qqy install git cmake wget python3-dev python3-pip python3-setuptools python3-tk python3-wheel xz-utils file make
+apt -qqy install git cmake wget ninja-build python3-dev python3-pip python3-setuptools python3-tk python3-wheel xz-utils file make
 pip3 install --upgrade pip
 pip3 install -r requirements_build.txt

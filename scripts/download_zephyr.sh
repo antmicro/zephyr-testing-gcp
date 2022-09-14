bddcda3d708affefcbedeaac5ec41cbc3ab47105
Krzysztof Sychla <ksychla5@gmail.com>
2022-09-14 14:50:21 +0200

    Bump zephyr to HEAD

--- scripts/download_zephyr.sh
+++ scripts/download_zephyr.sh
@@ -9,12 +9,11 @@ west init zephyrproject
 mkdir -p zephyr-sdk && cd zephyr-sdk
 curl --no-progress-meter -kL https://dl.antmicro.com/projects/renode/zephyr-sdk-${ZEPHYR_SDK_VERSION}_linux-x86_64.tar.gz | tar xz --strip 1
 cd -
-HEAD=8c1efa8b96
 LAST_COMMIT=$(cat last_zephyr_commit)
-COMMITS=$(git -C zephyrproject/zephyr log --pretty=format:'%h' $LAST_COMMIT..$HEAD)
+COMMITS=$(git -C zephyrproject/zephyr log --pretty=format:'%h' $LAST_COMMIT..HEAD)
 
 cd zephyrproject/zephyr
-CURRENT_COMMIT=$(git rev-parse --short $HEAD~${ZEPHYR_COMMIT})
+CURRENT_COMMIT=$(git rev-parse --short HEAD~${ZEPHYR_COMMIT})
 if [[ ! " ${COMMITS[*]} " =~ $CURRENT_COMMIT ]]; then
 	echo "Commit has already been built"
 	exit 1
@@ -24,6 +23,6 @@ git apply ../../patches/zephyr/*.patch
 pip3 install -r scripts/requirements.txt
 cd ..
 for i in $(seq 1 5); do west update 1>>../artifacts/build.log 2>&1 && break || sleep 5; done
-west espressif install 1>>../artifacts/build.log 2>&1
+west zephyr-export install 1>>../artifacts/build.log 2>&1
 cd ..
-#tar czf zephyr.tar.gz zephyrproject zephyr-sdk
+tar czf zephyr.tar.gz zephyrproject zephyr-sdk
